5665c990548d319bd3a283ea4716a752fe72d861
kiv1995 <kevin.stessel@edu.fh-joanneum.at>
2016-11-10 20:10:03 +0100

    Mit Zurückbutton

--- app/src/main/java/com/example/kevin/mycalculator2/DisplayAboutActivity.java
+++ app/src/main/java/com/example/kevin/mycalculator2/DisplayAboutActivity.java
@@ -19,5 +19,9 @@ public class DisplayAboutActivity extends AppCompatActivity {
         TextView view=(TextView) findViewById(R.id.textView);
         view.setText(message);
     }
+    public void goBack(View view){
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
 
 }
